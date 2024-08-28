78574f2cea3f6a5f472098022216ae91ae9c32a2
Błażej Sowa <bsowa123@gmail.com>
2024-08-28 19:43:17 +0200

    Add initial ros startup files

---
+++ RaphaOS-image/files/etc/ros/setup.bash
@@ -0,0 +1,26 @@
+source /opt/ros/jazzy/setup.bash
+
+### Robot Configuration
+
+# Namespace of the robot
+# Affects all node namespaces (except the controller node) and URDF link names
+export ROBOT_NAMESPACE=""
+
+
+### Start scripts variables
+
+# Path to the launch file to start.
+LAUNCH_FILE="/etc/ros/robot.launch.xml"
+
+# Arguments passed to ros2 launch command
+LAUNCH_ARGS=""
+
+# Arguments passed to Micro-ROS agent
+UROS_AGENT_ARGS="serial -D /dev/serial0 -b 460800"
+
+
+### ROS Environment Variables
+
+#export ROS_DOMAIN_ID=10
+#export ROS_LOCALHOST_ONLY=1
+export RCUTILS_COLORIZED_OUTPUT=1
