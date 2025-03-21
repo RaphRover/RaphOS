51349cb2c191c7ba9047b30f04ff9a4555b11bfb
Błażej Sowa <bsowa123@gmail.com>
2025-03-21 16:43:53 +0100

    Update ubuntu packages

--- OS-image/default.nix
+++ OS-image/default.nix
@@ -25,7 +25,7 @@ let
   scripts = pkgs.callPackage ./scripts { inherit files; };
 
   packageLists = let
-    noble-updates-stamp = "20250227T120000Z";
+    noble-updates-stamp = "20250321T120000Z";
     ros2-stamp = "2025-01-20";
     fictionlab-stamp = "2024-07-22";
   in [
@@ -49,8 +49,8 @@ let
       name = "noble-updates-main";
       packagesFile = (fetchurl {
         url =
-          "http://snapshot.ubuntu.com/ubuntu/20250227T120000Z/dists/noble-updates/main/binary-amd64/Packages.xz";
-        sha256 = "sha256-4trmJ96kCY/fCgIxWqQnZ3hKVLnGu4WsxH0VLsgl7Ck=";
+          "http://snapshot.ubuntu.com/ubuntu/${noble-updates-stamp}/dists/noble-updates/main/binary-amd64/Packages.xz";
+        sha256 = "sha256-WtDuyI0wf8KxzCNFtoPwqio5QMAK/YaL/f7uFjQH1A4=";
       });
       urlPrefix = "http://snapshot.ubuntu.com/ubuntu/${noble-updates-stamp}";
     }
@@ -58,8 +58,8 @@ let
       name = "noble-updates-universe";
       packagesFile = (fetchurl {
         url =
-          "http://snapshot.ubuntu.com/ubuntu/20250227T120000Z/dists/noble-updates/universe/binary-amd64/Packages.xz";
-        sha256 = "sha256-i7V6ZKKSqDq6AEVKZ9Mkko1qKZsTvGBwJZ8L464XBXs=";
+          "http://snapshot.ubuntu.com/ubuntu/${noble-updates-stamp}/dists/noble-updates/universe/binary-amd64/Packages.xz";
+        sha256 = "sha256-Myrfr+ChTxAiWSyyhVJiF8YmTUvtki6rIpxKPG9wz9c=";
       });
       urlPrefix = "http://snapshot.ubuntu.com/ubuntu/${noble-updates-stamp}";
     }
