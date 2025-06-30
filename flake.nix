a8b80169c32681510d8d42e9cf66c7174f58bf29
Błażej Sowa <bsowa123@gmail.com>
2025-06-30 17:56:39 +0200

    1.0.0

--- flake.nix
+++ flake.nix
@@ -9,7 +9,7 @@
       pkgs = (import nixpkgs) { inherit system; };
 
       OSName = "IbisOS";
-      OSVersion = "0.5.1";
+      OSVersion = "1.0.0";
 
       OSImage = pkgs.callPackage ./OS-image { inherit OSName OSVersion; };
 
