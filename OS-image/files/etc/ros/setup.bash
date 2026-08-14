2479361ef276c1052b03c3ddbc87f226154712cb
Jan Hernas <40989249+jasiex01@users.noreply.github.com>
2026-08-14 12:43:43 +0200

    feat: Add parameter overrides (#10)
    * Add example param overrides file
    
    * Add default param overrides file path to robot.launch
    
    * Clarify how to apply robot namespace
    
    * New filter node name in example overrides
    
    * Replace stale box_filter references

--- OS-image/files/etc/ros/setup.bash
+++ OS-image/files/etc/ros/setup.bash
@@ -9,6 +9,7 @@ source /etc/ros/aliases
 
 # Namespace of the robot
 # Affects all node namespaces and URDF link names
+# To apply the namespace, set this variable and run ros-restart or reboot the robot.
 export ROBOT_NAMESPACE=""
 
 
